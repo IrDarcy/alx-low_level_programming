17c01de7a5b40540126624ac6d49e5cf9b470662
IrDarcy <irdondarcy33@gmail.com>
2022-12-23 12:54:24 -0800

    strcpy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates 
- * @dest: string appended 
- * @src: string to be concatenated upon
- *
- * Retrun: 0
+ * *_strcat - concatenates
+ * @dest: string appended
+ * @src: string concantenated
+ * Retrun: void
  */
 
 char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+int a = -1, i;
+for (i = 0; dest[i] != '\0';)
+	i++;
+do {
+a++;
+dest[i] = src[a];
+i++;
+
+} while (src[a] != '\0');
+
 return (dest);
 }
